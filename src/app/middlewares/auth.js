6854dfba10ac93f6ef24e29bbca481d538fa6100
Júnior de Oliveira <ellismarjr@hotmail.com>
2019-10-15 16:20:11 -0300

    Recreate Git repository

---
+++ src/app/middlewares/auth.js
@@ -0,0 +1,28 @@
+import jwt from 'jsonwebtoken';
+import { promisify } from 'util';
+
+import authConfig from '../../config/auth';
+
+export default async (req, res, next) => {
+  const authHeader = req.headers.authorization;
+
+  // se o token não foi enviado, retornará um erro
+  if (!authHeader) {
+    return res.status(401).json({ error: 'Token not provided' });
+  }
+
+  /*
+    Separa o token em um array
+  */
+  const [, token] = authHeader.split(' ');
+
+  try {
+    const decoded = await promisify(jwt.verify)(token, authConfig.secret);
+
+    req.userId = decoded.id;
+
+    return next();
+  } catch (err) {
+    return res.status(401).json({ error: 'Token invalid' });
+  }
+};
---
+++ src/database/index.js
@@ -0,0 +1,38 @@
+// Esse arquivo é responsavel de conectar com o banco  e carregar os models
+import Sequelize from 'sequelize';
+import mongoose from 'mongoose';
+
+import User from '../app/models/User';
+import File from '../app/models/File';
+import Appointment from '../app/models/Appointment';
+
+import databaseConfig from '../config/database';
+
+const models = [User, File, Appointment];
+
+class Database {
+  constructor() {
+    this.init();
+    this.mongo();
+  }
+
+  init() {
+    // faz a conexão com o banco de dados
+    this.connection = new Sequelize(databaseConfig);
+
+    // percore o array e retorna os models
+    models
+      .map(model => model.init(this.connection))
+      .map(model => model.associate && model.associate(this.connection.models));
+  }
+
+  mongo() {
+    this.mongoConnection = mongoose.connect(process.env.MONGO_URL, {
+      useNewUrlParser: true,
+      useFindAndModify: true,
+      useUnifiedTopology: true,
+    });
+  }
+}
+
+export default new Database();
